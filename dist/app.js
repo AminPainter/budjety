857d43f5ef53870df5ed5df734825a0d17c2920e
AminPainter <aminpainter786@gmail.com>
2022-08-21 12:25:48 +0530

    reverted back to es2016

--- dist/app.js
+++ dist/app.js
@@ -1,44 +1,39 @@
 "use strict";
-var BudgetController = (function () {
-    var Item = /** @class */ (function () {
-        function Item(description, amount, itemType) {
+const BudgetController = (() => {
+    class Item {
+        constructor(description, amount, itemType) {
             this.description = description;
             this.amount = amount;
             this.itemType = itemType;
             this.id = ++Item.counter;
         }
-        Item.counter = 0;
-        return Item;
-    }());
-    var database = { incomes: [], expenses: [] };
+    }
+    Item.counter = 0;
+    const database = { incomes: [], expenses: [] };
     return {
-        addListItem: function (item) {
-            var newItem = new Item(item.description, item.amount, item.itemType);
+        addListItem(item) {
+            const newItem = new Item(item.description, item.amount, item.itemType);
             database[item.itemType === 'inc' ? 'incomes' : 'expenses'].push(newItem);
             return newItem;
         },
-        calcBudget: function () {
-            var totalIncome = database.incomes.reduce(function (accumulatedValue, currentExpense) {
-                return accumulatedValue + currentExpense.amount;
-            }, 0);
-            var totalExpense = database.expenses.reduce(function (accumulatedValue, currentExpense) {
-                return accumulatedValue + currentExpense.amount;
-            }, 0);
+        calcBudget() {
+            const totalIncome = database.incomes.reduce((accumulatedValue, currentExpense) => accumulatedValue + currentExpense.amount, 0);
+            const totalExpense = database.expenses.reduce((accumulatedValue, currentExpense) => accumulatedValue + currentExpense.amount, 0);
             return {
                 income: totalIncome,
                 expense: totalExpense,
                 budget: Math.max(totalIncome - totalExpense, 0),
             };
         },
-        removeListItem: function (itemType, itemId) {
-            var list = database[itemType === 'inc' ? 'incomes' : 'expenses'];
-            var indexOfItemToBeRemoved = list.findIndex(function (listItem) { return listItem.id === itemId; });
+        removeListItem(itemType, itemId) {
+            const list = database[itemType === 'inc' ? 'incomes' : 'expenses'];
+            const indexOfItemToBeRemoved = list.findIndex(listItem => listItem.id === itemId);
             list.splice(indexOfItemToBeRemoved, 1);
         },
     };
 })();
-var UIController = (function () {
-    var months = [
+const UIController = (() => {
+    const months = [
         'January',
         'February',
         'March',
@@ -52,7 +47,7 @@ var UIController = (function () {
         'November',
         'December',
     ];
-    var DOMElements = {
+    const DOMElements = {
         dateDisk: document.querySelector('.display__date'),
         budgetDisk: document.querySelector('.display__budget'),
         incomeTotalDisk: document.querySelector('.tab--inc .tab__amount'),
@@ -69,63 +64,69 @@ var UIController = (function () {
         get DOM() {
             return DOMElements;
         },
-        displayDate: function () {
-            var today = new Date();
-            DOMElements.dateDisk.textContent = "Available budget in ".concat(months[today.getMonth()], ", ").concat(today.getFullYear());
+        displayDate() {
+            const today = new Date();
+            DOMElements.dateDisk.textContent = `Available budget in ${months[today.getMonth()]}, ${today.getFullYear()}`;
         },
-        addListItem: function (item) {
-            var markup = "\n\t\t\t\t<li class=\"item\" id=\"".concat(item.itemType, "-").concat(item.id, "\" data-type=\"").concat(item.itemType, "\" data-id=\"").concat(item.id, "\">\n          <span class=\"item__description\">").concat(item.description, "</span>\n          <span class=\"item__amount\">\u20B9").concat(item.amount, "</span>\n          <button class=\"item__remove-btn\">&times;</button>\n        </li>\n\t\t\t");
+        addListItem(item) {
+            const markup = `
+				<li class="item" id="${item.itemType}-${item.id}" data-type="${item.itemType}" data-id="${item.id}">
+          <span class="item__description">${item.description}</span>
+          <span class="item__amount">₹${item.amount}</span>
+          <button class="item__remove-btn">&times;</button>
+        </li>
+			`;
             (item.itemType === 'inc'
                 ? DOMElements.incomesList
                 : DOMElements.expensesList).insertAdjacentHTML('beforeend', markup);
         },
-        updateBudget: function (budget) {
-            DOMElements.incomeTotalDisk.textContent = "\u20B9".concat(budget.income);
-            DOMElements.expenseTotalDisk.textContent = "\u20B9".concat(budget.expense);
-            DOMElements.budgetDisk.textContent = "\u20B9".concat(budget.budget);
+        updateBudget(budget) {
+            DOMElements.incomeTotalDisk.textContent = `₹${budget.income}`;
+            DOMElements.expenseTotalDisk.textContent = `₹${budget.expense}`;
+            DOMElements.budgetDisk.textContent = `₹${budget.budget}`;
         },
-        clearInputs: function () {
+        clearInputs() {
             DOMElements.descInput.value = DOMElements.amountInput.value = '';
             DOMElements.descInput.focus();
         },
-        removeListItem: function (listItemElement) {
+        removeListItem(listItemElement) {
             listItemElement.remove();
         },
     };
 })();
-var AppController = (function (uiCtrl, budgetCtrl) {
-    var DOM = UIController.DOM;
-    var addListItem = function (e) {
+const AppController = ((uiCtrl, budgetCtrl) => {
+    const { DOM } = UIController;
+    const addListItem = (e) => {
         e.preventDefault();
-        var amount = +DOM.amountInput.value;
-        var description = DOM.descInput.value;
+        const amount = +DOM.amountInput.value;
+        const description = DOM.descInput.value;
         if (!amount || !description)
             return;
-        var item = {
-            amount: amount,
-            description: description,
+        const item = {
+            amount,
+            description,
             itemType: DOM.choiceInput.value,
         };
-        var listItemWithId = budgetCtrl.addListItem(item);
+        const listItemWithId = budgetCtrl.addListItem(item);
         uiCtrl.addListItem(listItemWithId);
-        var budget = budgetCtrl.calcBudget();
+        const budget = budgetCtrl.calcBudget();
         uiCtrl.updateBudget(budget);
         uiCtrl.clearInputs();
     };
-    var removeListItem = function (e) {
+    const removeListItem = (e) => {
         var _a;
-        var target = e.target;
+        const target = e.target;
         if (!((_a = target.className) === null || _a === void 0 ? void 0 : _a.includes('item__remove-btn')))
             return;
-        var listItemElement = target.closest('.item');
+        const listItemElement = target.closest('.item');
         uiCtrl.removeListItem(listItemElement);
-        var _b = listItemElement.dataset, itemType = _b.type, itemId = _b.id;
+        const { type: itemType, id: itemId } = listItemElement.dataset;
         budgetCtrl.removeListItem(itemType, +itemId);
-        var budget = budgetCtrl.calcBudget();
+        const budget = budgetCtrl.calcBudget();
         uiCtrl.updateBudget(budget);
     };
     return {
-        init: function () {
+        init() {
             uiCtrl.displayDate();
             DOM.itemForm.addEventListener('submit', addListItem);
             DOM.itemsContainer.addEventListener('click', removeListItem);
